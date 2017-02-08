c0b199725d2a650715a1c6b859f383d07d737a5d
asinha872 <asinha872@gmail.com>
2017-02-08 13:26:07 +0530

    REsolved bugs with nltk

--- db/db/settings.py
+++ db/db/settings.py
@@ -15,7 +15,7 @@ Settings.
 LANGUAGE = "sparql"
 
 # NLTK config
-NLTK_DATA_PATH = [home/amit/Work/]  # List of paths with NLTK data
+NLTK_DATA_PATH = ['/home/amit/Work/FinalYearCSE/data']  # List of paths with NLTK data
 
 # Encoding config
 DEFAULT_ENCODING = "utf-8"
@@ -25,7 +25,7 @@ SPARQL_PREAMBLE = u"""
 PREFIX owl: <http://www.w3.org/2002/07/owl#>
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+PREFIX foaf: <http://xmlns.com/foaf/0.1/>0
 PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
 PREFIX quepy: <http://www.machinalis.com/quepy#>
 """
--- db/main.py
+++ db/main.py
@@ -6,5 +6,8 @@ Main script for db quepy.
 
 import quepy
 db = quepy.install("db")
-target, query, metadata = db.get_query("what is a blowtorch?")
+
+question = raw_input("Type your query: ")
+print(question)
+target, query, metadata = db.get_query(str(question))
 print query
