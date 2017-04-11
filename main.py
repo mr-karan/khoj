a51f59bf547730c3f6851c8fd6b4cf5250246209
Karan Sharma <karan.sharma@zerodha.com>
2017-04-11 10:58:11 +0530

    Change UI based on feedback

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from datetime import datetime
 from flask import Flask
-from flask import request, render_template
+from flask import request, render_template, session
 
 from get_answer import result
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
@@ -14,9 +13,20 @@ def index():
         return render_template('index.html')
     answer = result(username)[0]
     query_text = result(username)[1]
+    session['query_text'] = query_text
+
     if result:
         return render_template('result.html', answer=answer, query_text=query_text)
     return render_template('index.html', error_message=error_message)
 
+@app.route('/query')
+def check():
+    query_text = session.get('query_text', None)
+    return render_template('query.html', query_text=query_text)
+
+
 if __name__ == '__main__':
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'filesystem'
+
     app.run(debug=True)
